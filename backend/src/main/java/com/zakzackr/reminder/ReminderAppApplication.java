e3d6ea9dfd25bdabb6e4817b5761424cf98cf0a3
zakzackr <ryuichi12150105syhrh@gmail.com>
2023-09-28 19:50:44 +0900

    Initial Commit

---
+++ backend/src/main/java/com/zakzackr/reminder/ReminderAppApplication.java
@@ -0,0 +1,13 @@
+package com.zakzackr.reminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReminderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReminderAppApplication.class, args);
+	}
+
+}
