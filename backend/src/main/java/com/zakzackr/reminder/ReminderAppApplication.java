86d7d9a809b02e29b76451cf35d81caada61501f
zakzackr <ryuichi12150105syhrh@gmail.com>
2025-04-20 14:30:04 +0900

    refactored db schema

--- backend/src/main/java/com/zakzackr/reminder/ReminderAppApplication.java
+++ backend/src/main/java/com/zakzackr/reminder/ReminderAppApplication.java
@@ -1,10 +1,14 @@
 package com.zakzackr.reminder;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.zakzackr.reminder.entity.Role;
+import com.zakzackr.reminder.repository.RoleRepository;
 
 @SpringBootApplication
 @EnableScheduling
@@ -14,6 +18,18 @@ public class ReminderAppApplication {
 	public ModelMapper modelMapper(){
 		return new ModelMapper();
 	}
+
+	@Bean
+    CommandLineRunner initRoles(RoleRepository roleRepository) {
+        return args -> {
+            if (roleRepository.findByRole("ROLE_ADMIN") == null) {
+                roleRepository.save(new Role(1L, "ROLE_ADMIN"));
+            }
+            if (roleRepository.findByRole("ROLE_USER") == null) {
+                roleRepository.save(new Role(2L, "ROLE_USER"));
+            }
+        };
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReminderAppApplication.class, args);
