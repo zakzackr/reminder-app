40b4b7c2491374b5f627cead4f2f48b9c98512b9
zakzackr <ryuichi12150105syhrh@gmail.com>
2025-04-20 13:06:50 +0900

    Add an index to the user_id column in the reminders table

--- backend/src/main/java/com/zakzackr/reminder/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/zakzackr/reminder/security/JwtAuthenticationFilter.java
@@ -2,7 +2,6 @@ package com.zakzackr.reminder.security;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
